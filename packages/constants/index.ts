6312f34fd7b40f8889e6f50b5b43ecaa1744b857
whistleyz <whistleyz@163.com>
2023-02-12 18:04:07 +0800

    chore: add google analytics

--- packages/constants/index.ts
+++ packages/constants/index.ts
@@ -1,5 +1,7 @@
 import PROMPTS from '@/scripts/prompts.json';
 
+export const __DEV__ = process.env.NODE_ENV === 'development';
+
 export type Prompt = {
   act: string;
   prompt: string;
@@ -17,6 +19,10 @@ export const chatgptConstants = {
   apiKeys: getApiKeys(),
   maxContinuousCount: 10,
   promptSuffix: `\n\n${ASSISTANT_LABEL_DEFAULT}:\n`,
+};
+
+export const siteConstants = {
+  ga: process.env.SITE_GOOGLE_ANALYTICS,
 };
 
 function getApiKeys() {
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { siteConstants, __DEV__ } from '@/packages/constants';
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const SCRIPT = `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-NWXQDEPRCW');`;
 
 export default function Document() {
+  const analytics = !__DEV__ && siteConstants.ga && (
+    <>
+      <script async src={siteConstants.ga}></script>
+      <script>{SCRIPT}</script>
+    </>
+  );
+
   return (
     <Html lang="en">
       <Head />
       <body>
         <Main />
         <NextScript />
+        {analytics}
       </body>
     </Html>
-  )
+  );
 }
